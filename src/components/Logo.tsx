b8beb58edf0d3517cb80521dc1e6f0d38dc373d5
Teddy <teddy@Teddys-MacBook-Air.local>
2024-12-08 08:21:18 +0300

    Update

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,13 @@
-// import Logo from "../../public/logobluebird.png";
-
 export function Logo() {
   return (
     <div className="flex items-center">
-      <img src="../../public/logobluebird.png" alt="Logo" className="w-8 h-8" />
+      <span className="p-1 bg-white rounded-md">
+        <img
+          src="https://i.ibb.co/6NbDfhL/logobluebird.png"
+          alt="Logo"
+          className="w-8 h-8"
+        />
+      </span>
       <span className="ml-2 text-xl font-semibold text-white">
         Bluebird Millcreek
       </span>
